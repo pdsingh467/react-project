34fcfa74afaa071c326c9c184f84abc3435db601
pdsingh467 <singhpd467@gmail.com>
2024-11-16 12:19:15 +0530

    react initial code

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const heading = React.createElement("h1",{ id: "heading"},"hello world");
+const divElement1 =React.createElement("div",{id: "child"},heading,heading,heading);
+// console.log(heading);
+// const heading = React.createElement("h1",{ id: "heading"},"hello world");
+const divElement2 =React.createElement("div",)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(divElement);
